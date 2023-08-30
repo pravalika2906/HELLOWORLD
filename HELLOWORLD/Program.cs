844d04235cb257391ab56ef54c57bc4a4b4e05b7
rakeshrocky5430 <71966182+rakeshrocky5430@users.noreply.github.com>
2023-08-29 23:59:15 -0500

    Add project files.

---
+++ HELLOWORLD/Program.cs
@@ -0,0 +1,18 @@
+﻿using h = System.Console;
+
+namespace HELLOWORLD
+{
+    internal class Program
+    {
+        public void disp()
+        {
+            h.WriteLine("HELLO WORLD");
+        }
+        static void Main(string[] args)
+        {
+            h.WriteLine("GOOD MORNING");
+            Program obj=new Program();
+            obj.disp();
+        }
+    }
+}
